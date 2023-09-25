41858940e4542d3a64e711bef0d01873d5694795
syx <sunyuxuan@mail.ustc.edu.cn>
2023-09-25 12:11:50 +0800

    Update movie_spider.py

--- movie_spider.py
+++ movie_spider.py
@@ -30,7 +30,24 @@ cookies ={
     "Cookie": 'bid=UUJyGxdUAvE; __utmc=30149280; __utmz=30149280.1695283879.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); ll="118183"; ap_v=0,6.0; viewed="1046265_1017143_1084336_10466265_2256039_1007305"; apiKey=; dbcl2="274656538:3TdXAga9IPc"; ck=k-Tx; frodotk_db="a537afb8776aee52efa70f296640caf6"; push_noty_num=0; push_doumail_num=0; __utma=30149280.1842746003.1695283879.1695551068.1695556724.6; __utmv=30149280.27465; __utmt_douban=1; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1695557428%2C%22https%3A%2F%2Fbook.douban.com%2F%22%5D; _pk_id.100001.8cb4=c54a4c010270bb6c.1695557428.; _pk_ses.100001.8cb4=1; __yadk_uid=UW01ZWrMf8VOdGNYSersEnPau2Hod9ry; __utmt=1; __utmb=30149280.26.9.1695557430361'
 }
 
-
+def assign_none(movie):
+    movie = {
+    "title": None,
+    "year": None,
+    "director": None,
+    "genre": None,
+    "country": None,
+    "language": None,
+    "release date": None,
+    "aka": None,
+    "IMDb": None,
+    "rating": None,
+    "plot": None,
+    "cast": None,
+    "duration": None,
+    "id_recommendations": None
+}
+    
 # open the file to read and to save
 with open("Movie_id.csv", mode="r", encoding="utf-8") as movielist:
     # read the file
@@ -47,9 +64,13 @@ with open("Movie_id.csv", mode="r", encoding="utf-8") as movielist:
 
         # title
         title = soup.find("span", attrs={"property": "v:itemreviewed"})
-        print(title.text)
-
-        movie["title"] = str(title.text)
+        if title is None:
+            assign_none(movie)
+            print(str(row) + " is None")
+            continue
+        else: 
+            print(title.text)
+            movie["title"] = str(title.text)
 
         # year
         year = soup.find("span", attrs={"class": "year"})
@@ -59,9 +80,11 @@ with open("Movie_id.csv", mode="r", encoding="utf-8") as movielist:
 
         # director
         director = soup.find("a", attrs={"rel": "v:directedBy"})
-        print(director.text)
-
-        movie["director"] = str(director.text)
+        if director is None:
+            movie["director"] = None
+        else:
+            print(director.text)
+            movie["director"] = str(director.text)
 
         # genre
         genre = soup.find_all("span", attrs={"property": "v:genre"})
@@ -76,15 +99,21 @@ with open("Movie_id.csv", mode="r", encoding="utf-8") as movielist:
         movie["country"] = str(country_list)
 
         # language
-        language = soup.find("span", attrs={"class": "pl"}, text="语言:").next_sibling.strip()
-        language_list = language.split("/")
-        print(language_list)
-        movie["language"] = str(language_list)
+        language = soup.find("span", attrs={"class": "pl"}, text="语言:")
+        if language is None:
+            movie["language"] = None
+        else:
+            language_list = language.next_sibling.strip().split("/")
+            print(language_list)
+            movie["language"] = str(language_list)
 
         # release date
         release_date = soup.find("span", attrs={"property": "v:initialReleaseDate"})
-        print(release_date.text)
-        movie["release date"] = str(release_date.text)
+        if release_date is None:
+            movie["release date"] = None
+        else:
+            print(release_date.text)
+            movie["release date"] = str(release_date.text)
 
         # aka
         aka = soup.find("span", attrs={"class": "pl"}, text="又名:")
@@ -96,15 +125,21 @@ with open("Movie_id.csv", mode="r", encoding="utf-8") as movielist:
             movie["aka"] = str(aka_list)
 
         # IMDb
-        IMDb = soup.find("span", attrs={"class": "pl"}, text="IMDb:").next_sibling.strip()
-        print(IMDb)
-        movie["IMDb"] = str(IMDb)
+        IMDb = soup.find("span", attrs={"class": "pl"}, text="IMDb:")
+        if IMDb is None:
+            movie["IMDb"] = None
+        else :
+            IMDb_list = IMDb.next_sibling.strip()
+            print(IMDb_list)
+            movie["IMDb"] = str(IMDb_list)
 
         # rating
         rating = soup.find("strong", attrs={"class": "ll rating_num"})
-        print(rating.text)
-
-        movie["rating"] = str(rating.text)
+        if rating is None:
+                movie["rating"] = None
+        else:
+            print(rating.text)
+            movie["rating"] = str(rating.text)
 
         # plot
         plot = soup.find("span", class_="all hidden")
@@ -127,23 +162,32 @@ with open("Movie_id.csv", mode="r", encoding="utf-8") as movielist:
 
         # duration
         duration = soup.find("span", attrs={"property": "v:runtime"})
-        print(duration.text)
-        movie["duration"] = str(duration.text)
+        if duration is not None:
+            print(duration.text)
+            movie["duration"] = str(duration.text)
+        else:
+            duration=soup.find("span", attrs={"class": "pl"}, text={"单集片长:","片长:"}).next_sibling.strip()
+            print(duration)
+            movie["duration"] = str(duration)
+
 
         # id_recommendations
         patten = r"\d+/"
         recommendations_bd = soup.find("div", attrs={"class": "recommendations-bd"})
-        dd_tags = recommendations_bd.find_all("dd")
-        id_recommendations = []
-        # 遍历每个<a>标签，获取其href属性
-        for dd_tag in dd_tags:
-            a_tag = dd_tag.find("a")
-            href = a_tag["href"]
-            match = re.search(patten, href)
-            if match:
-                id_recommendations.append(match.group()[:-1])
-        print(id_recommendations)
-        movie["id_recommendations"] = str(id_recommendations)
+        if recommendations_bd is None:
+            movie["id_recommendations"] = None
+        else:
+            dd_tags = recommendations_bd.find_all("dd")
+            id_recommendations = []
+            # 遍历每个<a>标签，获取其href属性
+            for dd_tag in dd_tags:
+                a_tag = dd_tag.find("a")
+                href = a_tag["href"]
+                match = re.search(patten, href)
+                if match:
+                    id_recommendations.append(match.group()[:-1])
+            print(id_recommendations)
+            movie["id_recommendations"] = str(id_recommendations)
 
         # save the movie
         with open("Movies_Philip.json", "a", encoding="utf-8") as json_file:
