0156c33d574bb7d281c8e382768b998189662eac
PhilipGAQ <113370525+PhilipGAQ@users.noreply.github.com>
2023-10-14 20:24:27 +0800

    Update Fenci

--- FenCi_Philip.py
+++ FenCi_Philip.py
@@ -1,8 +1,27 @@
 import pynlpir
 import json
 
-pynlpir.open()
+pynlpir.open(encoding_errors="replace")
+Books_plot = {"title": "无人生还", "keyword": ""}
+
+with open("KeyWords_Philip.json", "a", encoding="utf-8") as jf:
+    jf.write("[")
+
 with open("Books_Philip.json", "r", encoding="utf-8") as json_file:
     data = json.load(json_file)
-    print(pynlpir.segment(data[0]["plot"], pos_tagging=False))
-    print(pynlpir.get_key_words(data[0]["plot"]))
+    for d in data:
+        Books_plot["title"] = d["title"]
+        if d["plot"] is not None:
+            # print(pynlpir.segment(d["plot"], pos_tagging=False))
+            print(pynlpir.get_key_words(d["plot"], max_words=50))
+            pynlpir.segment(d["plot"])
+            Books_plot["keyword"] = pynlpir.get_key_words(d["plot"])
+        else:
+            Books_plot["keyword"] = ""
+        with open("KeyWords_Philip.json", "a", encoding="utf-8") as jf:
+            json.dump(Books_plot, jf, indent=4, ensure_ascii=False)
+            jf.write(",\n")
+
+with open("KeyWords_Philip.json", "a", encoding="utf-8") as jf:
+    jf.write("]")
+pynlpir.close()
