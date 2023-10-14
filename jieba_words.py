3146b37b54f053fb3f1bd5b6d105fc8992d968a6
Jingfan Hu <hujingfan2004@mail.ustc.edu.cn>
2023-10-14 20:45:56 +0800

    Update jieba_words.py

--- jieba_words.py
+++ jieba_words.py
@@ -5,17 +5,27 @@ import jieba
 with open('Books_Philip.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
+book_words = {
+    "num": "1",
+    "title": "The little prince",
+    "writer": "Antoine de Saint-Exupéry",
+    "words": ["一"]
+}
+
+
 # 分词
 for i in range(0,len(data)):
     title = data[i]['title']
+    book_words["title"] = title
+    writer = data[i]['writer']
+    book_words["writer"] = writer
+    book_words["num"] = i
     plot = data[i]['plot']
     if(plot == None):
         continue
     words = jieba.lcut(plot)
     words = [word for word in words if '\u4e00' <= word <= '\u9fa5']  # 只保留中文
-    print(i)
-    print("\n")
-    print(title)
-    print(":")
-    print(words)
-    print("\n")
+    book_words["words"] = words  # 将分词结果拼接成一个字符串
+    with open("book_jieba_words.json", "a", encoding="utf-8") as json_file:
+        json.dump(book_words, json_file, indent=4, ensure_ascii=False)
+
