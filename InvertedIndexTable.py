59ea582ea91fb211bc47630ac4c07cc402c2d019
sunyuxuan <sunyuxuan@mail.ustc.edu.cn>
2023-10-26 16:04:32 +0800

    Update InvertedIndexTable.py

--- InvertedIndexTable.py
+++ InvertedIndexTable.py
@@ -2,6 +2,16 @@ import json
 from collections import defaultdict
 import math
 
+
+def create_skip_pointers(threshold, docs,skip_list,skip_index):
+    if len(docs) > threshold:
+        skip_interval = int(math.sqrt(len(docs)))
+        skip_list = [docs[i:i+skip_interval][-1] for i in range(0, len(docs), skip_interval)]
+        skip_index = [i+skip_interval-1 for i in range(0, len(docs), skip_interval)]
+        return skip_list, skip_index
+    else:
+        return [], []
+
 # Load the JSON file
 with open('TagsForBooks_Philip.json', 'r',encoding="utf-8") as f:
     data = json.load(f)
@@ -22,6 +32,7 @@ inverted_index = dict(sorted(inverted_index.items()))
 threshold = 10
 
 # Create a dictionary to store the skip pointers
+skip_pointers_index={}
 skip_pointers = {}
 
 # Loop through each tag and its document list in the inverted index
@@ -32,8 +43,10 @@ for tag, docs in inverted_index.items():
         skip_interval = int(math.sqrt(len(docs)))
         # Create a list of skip pointers
         skip_list = [docs[i:i+skip_interval][-1] for i in range(0, len(docs), skip_interval)]
+        skip_index = [i+skip_interval-1 for i in range(0, len(docs), skip_interval)]
         # Add the skip pointers to the dictionary
         skip_pointers[tag] = skip_list
+        skip_pointers_index[tag]=skip_index
 
 
 # Print the inverted index and skip pointers
@@ -42,6 +55,9 @@ print(inverted_index)
 print('\n')
 print("Skip Pointers:")
 print(skip_pointers)
+print('\n')
+print("Skip_Index:")
+print(skip_pointers_index)
 print('\n')'''
 
 def infix_to_postfix(query):
@@ -86,10 +102,64 @@ def infix_to_postfix(query):
     print(' '.join(output))
     return ' '.join(output)
 
+def bool_and(res1,res2,index1,index2):
+    results = []
+    cnt1=cnt2=0
+    '''print("res1", res1)
+    print("res2",res2)
+    print("index1",index1)
+    print("index2",index2)'''
+    if len(index1)==0 or len(index2)==0:
+        while cnt1<len(res1) and cnt2<len(res2):
+            if res1[cnt1]==res2[cnt2]:
+                results.append(res1[cnt1])
+                cnt1+=1
+                cnt2+=1
+            elif res1[cnt1]<res2[cnt2]: 
+                cnt1+=1
+            else:
+                cnt2+=1
+    else:
+        gap1=int(index1[1])-int(index1[0])
+        gap2=int(index2[1])-int(index2[0])
+        while cnt1<len(res1) and cnt2<len(res2):
+            if res1[cnt1]==res2[cnt2]:
+                results.append(res1[cnt1])
+                cnt1+=1
+                cnt2+=1
+            elif res1[cnt1]<res2[cnt2]:
+                if cnt1 not in index1 or cnt1+gap1>=len(res1):
+                    cnt1+=1
+                else:
+                    if res1[cnt1+gap1]<=res2[cnt2]:
+                        cnt1+=gap1
+                        continue
+                    else :
+                        cnt1+=1
+                        continue
+            else:
+                if cnt2 not in index2 or cnt2+gap2>=len(res2):
+                    cnt2+=1
+                else:
+                    if res2[cnt2+gap2]<=res1[cnt1]:
+                        cnt2+=gap2
+                        continue
+                    else :
+                        cnt2+=1
+                        continue
+    '''print("results:")
+    print(results)
+    print('\n')
+    return results'''
+
+
 def bool_search(query):
     # Split the query into individual terms
     terms = query.split()
     print(terms)
+    result_and=[]
+    result_and_pointers=[]
+    result_and_index=[]
 
     if len(terms) == 1:
         results = set(inverted_index.get(terms[0], []))
@@ -98,28 +168,41 @@ def bool_search(query):
         cnt=0
         res1 = set(inverted_index.get(terms[cnt], []))
         if terms[cnt+1]=="NOT":
-            results = set(range(1, 1001))-res1
+            results = set(range(1, 1201))-res1
+            result_and=list(results)
+            result_and_pointers,result_and_index=create_skip_pointers(10,result_and,result_and_pointers,result_and_index)
             cnt+=2
         else:
             res2 = set(inverted_index.get(terms[cnt+1], []))
             if terms[cnt+2]=="AND":
-                results = res1 & res2
+                result_and = bool_and(inverted_index.get(terms[cnt],[]),inverted_index.get(terms[cnt+1],[]),skip_pointers_index.get(terms[cnt],[]),skip_pointers_index.get(terms[cnt+1],[]))
+                result_and_pointers,result_and_index=create_skip_pointers(10,result_and,result_and_pointers,result_and_index)
+                results=set(result_and)
             elif terms[cnt+2]=="OR":
                 results = res1 | res2
+                result_and=list(results)
+                result_and_pointers,result_and_index=create_skip_pointers(10,result_and,result_and_pointers,result_and_index)
             else :
                 print("Invalid query")
                 return []
             cnt+=3
         while cnt<len(terms):
             if terms[cnt]=="NOT":
-                results = set(range(1, 1001))-results
+                results = set(range(1, 1201))-results
+                result_and=list(results)
+                result_and_pointers,result_and_index=create_skip_pointers(10,result_and,result_and_pointers,result_and_index)
+                print("index:",result_and_index)
                 cnt+=1
             else:
                 res = set(inverted_index.get(terms[cnt], []))
                 if terms[cnt+1]=="AND":
-                    results = results & res
+                    result_and = bool_and(inverted_index.get(terms[cnt],[]),result_and,skip_pointers_index.get(terms[cnt],[]),result_and_index)
+                    result_and_pointers,result_and_index=create_skip_pointers(10,result_and,result_and_pointers,result_and_index)
+                    results=set(result_and)
                 elif terms[cnt+1]=="OR":
                     results = results | res
+                    result_and=list(results)
+                    result_and_pointers,result_and_index=create_skip_pointers(10,result_and,result_and_pointers,result_and_index)
                 else :
                     print("Invalid query")
                     return []
