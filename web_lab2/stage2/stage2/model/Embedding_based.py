d4325b63a69d59172161bd6c472279bd2c856b98
PhilipGAQ <113370525+PhilipGAQ@users.noreply.github.com>
2023-12-21 19:43:31 +0800

    Update Embedding_based.py

--- web_lab2/stage2/stage2/model/Embedding_based.py
+++ web_lab2/stage2/stage2/model/Embedding_based.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import numpy as np
 
 def _L2_loss_mean(x):
     return torch.mean(torch.sum(torch.pow(x, 2), dim=1, keepdim=False) / 2.)
@@ -40,45 +40,43 @@ class Embedding_based(nn.Module):
         # TransR 
         self.trans_M = nn.Parameter(torch.Tensor(self.n_relations, self.embed_dim, self.relation_dim))
         nn.init.xavier_uniform_(self.trans_M)
+    '''
+        def calc_kg_loss_TransR(self, h, r, pos_t, neg_t):
+            """
+            h:      (kg_batch_size)
+            r:      (kg_batch_size)
+            pos_t:  (kg_batch_size)
+            neg_t:  (kg_batch_size)
+            """
+            r_embed = self.relation_embed(r)                                                # (kg_batch_size, relation_dim)
+            W_r = self.trans_M[r]                                                           # (kg_batch_size, embed_dim, relation_dim)
 
+            h_embed = self.entity_embed(h)                                                  # (kg_batch_size, embed_dim)
+            pos_t_embed = self.entity_embed(pos_t)                                          # (kg_batch_size, embed_dim)
+            neg_t_embed = self.entity_embed(neg_t)                                          # (kg_batch_size, embed_dim)
 
-    def calc_kg_loss_TransR(self, h, r, pos_t, neg_t):
-        """
-        h:      (kg_batch_size)
-        r:      (kg_batch_size)
-        pos_t:  (kg_batch_size)
-        neg_t:  (kg_batch_size)
-        """
-        r_embed = self.relation_embed(r)                                                # (kg_batch_size, relation_dim)
-        W_r = self.trans_M[r]                                                           # (kg_batch_size, embed_dim, relation_dim)
+            # 1. 计算头实体，尾实体和负采样的尾实体在对应关系空间中的投影嵌入
+            r_mul_h = r_mul_h/np.linalg.norm(r_mul_h, ord=2, axis=-1, keepdims=True)                                                    # (kg_batch_size, relation_dim)
+            r_mul_pos_t =                                                    # (kg_batch_size, relation_dim)
+            r_mul_neg_t =                                                                   # (kg_batch_size, relation_dim)
 
-        h_embed = self.entity_embed(h)                                                  # (kg_batch_size, embed_dim)
-        pos_t_embed = self.entity_embed(pos_t)                                          # (kg_batch_size, embed_dim)
-        neg_t_embed = self.entity_embed(neg_t)                                          # (kg_batch_size, embed_dim)
+            # 2. 对关系嵌入,头实体嵌入,尾实体嵌入,负采样的尾实体嵌入进行L2范数归一化
+            r_embed = 
+            r_mul_h = 
+            r_mul_pos_t = 
+            r_mul_neg_t = 
 
-        # 1. 计算头实体，尾实体和负采样的尾实体在对应关系空间中的投影嵌入
-        r_mul_h=                                                                        # (kg_batch_size, relation_dim)
-        r_mul_pos_t =                                                                   # (kg_batch_size, relation_dim)
-        r_mul_neg_t =                                                                   # (kg_batch_size, relation_dim)
+            # 3. 分别计算正样本三元组 (h_embed, r_embed, pos_t_embed) 和负样本三元组 (h_embed, r_embed, neg_t_embed) 的得分
+            pos_score =                                                                     # (kg_batch_size)
+            neg_score =                                                                     # (kg_batch_size)
 
-        # 2. 对关系嵌入，头实体嵌入，尾实体嵌入，负采样的尾实体嵌入进行L2范数归一化
-        r_embed=
-        r_mul_h = 
-        r_mul_pos_t = 
-        r_mul_neg_t = 
+            # 4. 使用 BPR Loss 进行优化，尽可能使负样本的得分大于正样本的得分
+            kg_loss = 
 
-        # 3. 分别计算正样本三元组 (h_embed, r_embed, pos_t_embed) 和负样本三元组 (h_embed, r_embed, neg_t_embed) 的得分
-        pos_score =                                                                     # (kg_batch_size)
-        neg_score =                                                                     # (kg_batch_size)
-
-        # 4. 使用 BPR Loss 进行优化，尽可能使负样本的得分大于正样本的得分
-        kg_loss=
-
-        l2_loss = _L2_loss_mean(r_mul_h) + _L2_loss_mean(r_embed) + _L2_loss_mean(r_mul_pos_t) + _L2_loss_mean(r_mul_neg_t)
-        loss = kg_loss + self.kg_l2loss_lambda * l2_loss
-        return loss
-
-
+            l2_loss = _L2_loss_mean(r_mul_h) + _L2_loss_mean(r_embed) + _L2_loss_mean(r_mul_pos_t) + _L2_loss_mean(r_mul_neg_t)
+            loss = kg_loss + self.kg_l2loss_lambda * l2_loss
+            return loss
+    '''
     def calc_kg_loss_TransE(self, h, r, pos_t, neg_t):
         """
         h:      (kg_batch_size)
@@ -93,17 +91,17 @@ class Embedding_based(nn.Module):
         neg_t_embed = self.entity_embed(neg_t)                                          # (kg_batch_size, embed_dim)
 
         # 5. 对关系嵌入，头实体嵌入，尾实体嵌入，负采样的尾实体嵌入进行L2范数归一化
-        r_embed = 
-        h_embed = 
-        pos_t_embed = 
-        neg_t_embed = 
+        r_embed = r_embed/ np.linalg.norm(r_embed, ord=2, axis=-1, keepdims=True)
+        h_embed = h_embed/np.linalg.norm(h_embed, ord=2, axis=-1, keepdims=True)
+        pos_t_embed = pos_t_embed/np.linalg.norm(pos_t_embed, ord=2, axis=-1, keepdims=True)
+        neg_t_embed = neg_t_embed/np.linalg.norm(neg_t_embed, ord=2, axis=-1, keepdims=True)
 
         # 6. 分别计算正样本三元组 (h_embed, r_embed, pos_t_embed) 和负样本三元组 (h_embed, r_embed, neg_t_embed) 的得分
-        pos_score =                                                                      # (kg_batch_size)
-        neg_score =                                                                      # (kg_batch_size)
+        pos_score =np.linalg.norm(h_embed + r_embed - pos_t_embed, ord=2)                                                                # (kg_batch_size)
+        neg_score =np.linalg.norm(h_embed + r_embed - neg_t_embed, ord=2)                                          # (kg_batch_size)
 
         # 7. 使用 BPR Loss 进行优化，尽可能使负样本的得分大于正样本的得分
-        kg_loss = 
+        kg_loss = -torch.log(nn.Sigmoid(pos_score - neg_score)).sum()
 
         l2_loss = _L2_loss_mean(h_embed) + _L2_loss_mean(r_embed) + _L2_loss_mean(pos_t_embed) + _L2_loss_mean(neg_t_embed)
         loss = kg_loss + self.kg_l2loss_lambda * l2_loss
@@ -124,8 +122,8 @@ class Embedding_based(nn.Module):
         item_neg_kg_embed = self.entity_embed(item_neg_ids)                             # (cf_batch_size, embed_dim)
         
         # 8. 为 物品嵌入 注入 实体嵌入的语义信息
-        item_pos_cf_embed =                                                             # (cf_batch_size, embed_dim)
-        item_neg_cf_embed =                                                             # (cf_batch_size, embed_dim)
+        item_pos_cf_embed =   torch.cat((item_pos_embed, item_pos_kg_embed), dim=1)                                                          # (cf_batch_size, embed_dim)
+        item_neg_cf_embed =   torch.cat((item_neg_embed,item_neg_kg_embed),dim=1)                                                         # (cf_batch_size, embed_dim)
 
         pos_score = torch.sum(user_embed * item_pos_cf_embed, dim=1)                    # (cf_batch_size)
         neg_score = torch.sum(user_embed * item_neg_cf_embed, dim=1)                    # (cf_batch_size)
@@ -172,7 +170,7 @@ class Embedding_based(nn.Module):
         item_kg_embed = self.entity_embed(item_ids)                                     # (n_items, embed_dim)
 
         # 9. 为 物品嵌入 注入 实体嵌入的语义信息
-        item_cf_embed =                                                                 # (n_items, embed_dim)
+        item_cf_embed =       torch.cat((item_embed,item_kg_embed),dim=1)                                                          # (n_items, embed_dim)
 
         cf_score = torch.matmul(user_embed, item_cf_embed.transpose(0, 1))              # (n_users, n_items)
         
